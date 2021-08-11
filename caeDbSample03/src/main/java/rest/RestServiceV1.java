5f334b571fab1c3b4ad338fd8621c55125417a0d
AURAjp <basket.3522@gmail.com>
2021-08-12 00:43:03 +0900

    調整

--- caeDbSample03/src/main/java/rest/RestServiceV1.java
+++ caeDbSample03/src/main/java/rest/RestServiceV1.java
@@ -22,85 +22,85 @@ import dao.Dao;
  */
 @Path("/v1")
 public class RestServiceV1 {
-	Dao dao = new Dao();
+    Dao dao = new Dao();
 
-	/**
-	 * <h5>全データの取得 (JSON/JSONP)</h5>
-	 * @param callback : 自動で入るcallback関数名 
-	 * @return 全てのデータ
-	 */
-	@Path("getAll")
-	@Produces({"application/x-javascript", "application/json"})
-	@JSONP(queryParam = "callback")
+    /**
+     * <h5>全データの取得 (JSON/JSONP)</h5>
+     * @param callback : 自動で入るcallback関数名 
+     * @return 全てのデータ
+     */
+    @Path("getAll")
+    @Produces({"application/x-javascript", "application/json"})
+    @JSONP(queryParam = "callback")
     @GET
     public String getAll(@QueryParam("callback") String callback) {
        return Json.createArrayBuilder(dao.getAll()).build().toString();
-	}
+    }
 
-	/**
-	 * <h5>1つデータを書き込む(GET版)</h5>
-	 * @param callback : 自動で入るcallback関数名
-	 * @param name : "name"データ
-	 * @param text : "text"データ
-	 * @param request : httpリクエストパラメータ
-	 * @return : 受信データ＋IPアドレス，myname
-	 * This version supports JSONP. 
-	 */
-	@Path("writeData")
-	@Produces({"application/x-javascript", "application/json"})
-	@JSONP(queryParam = "callback")
+    /**
+     * <h5>1つデータを書き込む(GET版)</h5>
+     * @param callback : 自動で入るcallback関数名
+     * @param name : "name"データ
+     * @param text : "text"データ
+     * @param request : httpリクエストパラメータ
+     * @return : 受信データ＋IPアドレス，myname
+     * This version supports JSONP. 
+     */
+    @Path("writeData")
+    @Produces({"application/x-javascript", "application/json"})
+    @JSONP(queryParam = "callback")
     @GET
     public String writeData(
             @QueryParam("callback") String callback,
             @QueryParam("company") String company,
-    		@QueryParam("name") String name,
-    		@QueryParam("price") String price,
-    		@Context HttpServletRequest request) {
+            @QueryParam("name") String name,
+            @QueryParam("price") String price,
+            @Context HttpServletRequest request) {
 
-		LinkedHashMap<String,Object> var = new LinkedHashMap<>();
-		var.put("company", escapeHtml(convertToOiginal(company)));
-		var.put("name"   , escapeHtml(convertToOiginal(name   )));
-		var.put("price"  , escapeHtml(convertToOiginal(price  )));
-		dao.addLine(var);
-		return Json.createObjectBuilder(var).build().toString();
- 	}
+        LinkedHashMap<String,Object> var = new LinkedHashMap<>();
+        var.put("company", escapeHtml(convertToOiginal(company)));
+        var.put("name"   , escapeHtml(convertToOiginal(name   )));
+        var.put("price"  , escapeHtml(convertToOiginal(price  )));
+        dao.addLine(var);
+        return Json.createObjectBuilder(var).build().toString();
+    }
 
-	/**
-	 * <h5>Unicodeを含むWebエンコードされた文字列から元の文字列に変換する ("%u3042" -> "あ", "%20" -> " ")</h5>
-	 * 参考サイト: http://qiita.com/sifue/items/039846cf8415efdc5c92
-	 * @param unicode
-	 * @return decoded string
-	 */
-	private static String convertToOiginal(String unicode)
-	{
-	    String[] codeStrs = unicode.split("%");
-	    String encodedText = codeStrs[0];
-  		int[] codePoints = new int[1];
-	    for(int i = 1; i < codeStrs.length; i++) {
-	    	if(codeStrs[i].startsWith("u")) {
-	    		codePoints[0] = Integer.parseInt(codeStrs[i].substring(1, 5), 16);
-	    		encodedText += new String(codePoints, 0, codePoints.length);
-	    		if(codeStrs[i].length() > 5) {
-	    			encodedText += codeStrs[i].substring(5,codeStrs[i].length());
-	    		}
-	    	} else {
-	    		codePoints[0] = Integer.parseInt(codeStrs[i].substring(0, 2), 16);
-	    		encodedText += new String(codePoints, 0, codePoints.length);
-	    		if(codeStrs[i].length() > 2) {
-	    			encodedText += codeStrs[i].substring(2,codeStrs[i].length());
-	    		}
-	    	}
-	    }
-	    return encodedText;
-	}
+    /**
+     * <h5>Unicodeを含むWebエンコードされた文字列から元の文字列に変換する ("%u3042" -> "あ", "%20" -> " ")</h5>
+     * 参考サイト: http://qiita.com/sifue/items/039846cf8415efdc5c92
+     * @param unicode
+     * @return decoded string
+     */
+    private static String convertToOiginal(String unicode)
+    {
+        String[] codeStrs = unicode.split("%");
+        String encodedText = codeStrs[0];
+        int[] codePoints = new int[1];
+        for(int i = 1; i < codeStrs.length; i++) {
+            if(codeStrs[i].startsWith("u")) {
+                codePoints[0] = Integer.parseInt(codeStrs[i].substring(1, 5), 16);
+                encodedText += new String(codePoints, 0, codePoints.length);
+                if(codeStrs[i].length() > 5) {
+                    encodedText += codeStrs[i].substring(5,codeStrs[i].length());
+                }
+            } else {
+                codePoints[0] = Integer.parseInt(codeStrs[i].substring(0, 2), 16);
+                encodedText += new String(codePoints, 0, codePoints.length);
+                if(codeStrs[i].length() > 2) {
+                    encodedText += codeStrs[i].substring(2,codeStrs[i].length());
+                }
+            }
+        }
+        return encodedText;
+    }
 
-	/**
-	 * escape HTML code / HTMLコードをエスケープする (& → &amp;, < → &gt; ...)
-	 * @param s code
-	 * @return escaped code
-	 */
-	private static String escapeHtml(String s) {
-		return s.replace("&", "&amp;").replace("\"", "&quot;").replace("<", "&lt;").replace(">", "&gt;").replace("'", "&#39;");
-	}
+    /**
+     * escape HTML code / HTMLコードをエスケープする (& → &amp;, < → &gt; ...)
+     * @param s code
+     * @return escaped code
+     */
+    private static String escapeHtml(String s) {
+        return s.replace("&", "&amp;").replace("\"", "&quot;").replace("<", "&lt;").replace(">", "&gt;").replace("'", "&#39;");
+    }
 
 }
--- caeDbSample03/src/main/webapp/js/api.js
+++ caeDbSample03/src/main/webapp/js/api.js
@@ -10,35 +10,36 @@ const uriForFile = "http://localhost:8080/caeDbSample03/";
  * jQuery: html(このファイルの末尾まで)が読み込まれた後で実行されるプロセス
  */
 $(function() {
-	showAllFootprints();
-	$('#myForm').submit(function(event) {
-		// HTMLでの送信をキャンセル
-		event.preventDefault();
-		submitDataGet();
-		//	submitDataPost();
-	});
+  showAllFootprints();
+  $('#myForm').submit(function(event) {
+    // HTMLでの送信をキャンセル
+    event.preventDefault();
+    submitDataGet();
+    //	submitDataPost();
+  });
 })
 
 /**
-	* <h5>REST型のAPIで同期的にJSONP型データを取得する</h5>
-	* JSONP型のWeb APIで全てのデータを取得し，下部に表示する．
-	*/
+  * <h5>REST型のAPIで同期的にJSONP型データを取得する</h5>
+  * JSONP型のWeb APIで全てのデータを取得し，下部に表示する．
+  */
 function showAllFootprints() {
-	let uri = "api/v1/getAll?callback=?";
-	if (location.href.substr(0, 4) == "file") {
-		uri = uriForFile + uri;
-	}
-	$.getJSON(uri, function(data) {
-		$('#allFootprints').children().remove();
-		for (const key in data) {
-			$('#allFootprints').append(
-				'<div style="margin: 5px; border:1px solid #ff0000; background-color: #ffeeee;">'
-				+ data[key]['id'] + ' <b>' + data[key]['name'] + '</b> ('
-				+ (new Date(data[key]['last']).toLocaleString()) + ' from ' + data[key]['ip'] + ')<br>'
-				+ '<div style="background-color: #ffffff; color: #666666;">'
-				+ data[key]['text'] + '</div></div>\n');
-		}
-	});
+  let uri = "api/v1/getAll?callback=?";
+  if (location.href.substr(0, 4) == "file") {
+    uri = uriForFile + uri;
+  }
+  $.getJSON(uri, function(data) {
+    $('#allFootprints').children().remove();
+
+  let html = "<table><thead><tr><th>ID</th><th>会社名</th><th>商品名</th><th>価格</th><th>最終更新日</th></tr></thead>";
+    for (const key in data) {
+      html += '<tbody><tr><td>' + data[key]['id'] + '</td><td>'
+  + [key]['company'] + '</td><td>' + data[key]['name'] + '</td><td>'
+  + [key]['price'] + '</td><td>' + (new Date(data[key]['last']).toLocaleString()) + '</td></tr></tbody>';
+    }
+  html += '</table>';
+  $('#allFootprints').append(html);
+  });
 }
 
 /**
@@ -46,19 +47,19 @@ function showAllFootprints() {
  * (クロスドメインで動作する)
  */
 function submitDataGet(addInfo) {
-	console.log("addInfo" + addInfo);
-	let uri = "api/v1/writeData?callback=?";
-	if (location.href.substr(0, 4) == "file") {
-		uri = uriForFile + uri;
-	}
-	// http://localhost:8080/caeDbSample03/api/v1/writeData?callback=jQuery112403590900840136737_1628691044451&name=vcxz&text=vzxcv&_=1628691044453
-	console.log("URL" + uri);
-	let parm = {}; // new Array()はダメ！ http://bobobo.bona.jp/blog/?p=16
-	parm["company"] = escape($('#myForm [name=company]').val());
-	parm["text"] = escape($('#myForm [name=text]').val());
-	if (addInfo != null) parm["text"] = parm["text"] + "(" + addInfo + ")";
-	$.getJSON(uri, parm, function(data) {
-		console.log("submitDataGet:" + JSON.stringify(data));
-		showAllFootprints();
-	});
+  let uri = "api/v1/writeData?callback=?";
+  if (location.href.substr(0, 4) == "file") {
+    uri = uriForFile + uri;
+  }
+
+  let parm = {}; // new Array()はダメ！ http://bobobo.bona.jp/blog/?p=16
+  parm["company"] = escape($('#myForm [name=company]').val());
+  parm["name"]    = escape($('#myForm [name=name]').val());
+  parm["price"]   = escape($('#myForm [name=price]').val());
+  if (addInfo != null) { parm["name"] = parm["name"] + "(" + addInfo + ")"; }
+  if (addInfo != null) { parm["price"] = parm["price"] + "(" + addInfo + ")"; }
+  $.getJSON(uri, parm, data => {
+    console.log("submitDataGet:" + JSON.stringify(data));
+    showAllFootprints();
+  });
 }
