05d2eda118dd6684ad11a00ff78821a32b122e7f
AURAjp <basket.3522@gmail.com>
2021-08-12 16:52:01 +0900

    変数名などを調整

--- caeDbSample03/src/main/webapp/js/api.js
+++ caeDbSample03/src/main/webapp/js/api.js
@@ -1,16 +1,10 @@
-/**
- * JavaScript for caeDbSample03
- * @version 20210707 @since 20190704
- * @author nakano@cc.kumamoto-u.ac.jp
- */
-
 const uriForFile = "http://localhost:8080/caeDbSample03/";
 
 /**
  * jQuery: html(このファイルの末尾まで)が読み込まれた後で実行されるプロセス
  */
 $(function() {
-  showAllFootprints();
+  showAllProducts();
   $('#myForm').submit(function(event) {
     // HTMLでの送信をキャンセル
     event.preventDefault();
@@ -23,13 +17,13 @@ $(function() {
   * <h5>REST型のAPIで同期的にJSONP型データを取得する</h5>
   * JSONP型のWeb APIで全てのデータを取得し，下部に表示する．
   */
-function showAllFootprints() {
+function showAllProducts() {
   let uri = "api/v1/getAll?callback=?";
   if (location.href.substr(0, 4) == "file") {
     uri = uriForFile + uri;
   }
   $.getJSON(uri, function(data) {
-    $('#allFootprints').children().remove();
+    $('#allProducts').children().remove();
   let html = "<table><thead><tr><th>ID</th><th>会社名</th><th>商品名</th><th>価格</th><th>最終更新日</th></tr></thead>";
     for (const key in data) {
       html += '<tbody><tr><td>' + data[key]['id'] + '</td><td>'
@@ -37,12 +31,12 @@ function showAllFootprints() {
   + data[key]['price'] + '</td><td>' + (new Date(data[key]['last']).toLocaleString()) + '</td></tr></tbody>';
     }
   html += '</table>';
-  $('#allFootprints').append(html);
+  $('#allProducts').append(html);
   });
 }
 
 /**
- * <h5>Web APIで1つのデータをGETで保存する (2)</h5>
+ * <h5>Web APIで1つのデータをGETで保存する</h5>
  * (クロスドメインで動作する)
  */
 function submitDataGet(addInfo) {
@@ -51,7 +45,7 @@ function submitDataGet(addInfo) {
     uri = uriForFile + uri;
   }
 
-  let parm = {}; // new Array()はダメ！ http://bobobo.bona.jp/blog/?p=16
+  let parm = {}; // new Array()はダメ！
   parm["company"] = escape($('#myForm [name=company]').val());
   parm["name"]    = escape($('#myForm [name=name]').val());
   parm["price"]   = escape($('#myForm [name=price]').val());
@@ -59,6 +53,6 @@ function submitDataGet(addInfo) {
   if (addInfo != null) { parm["price"] = parm["price"] + "(" + addInfo + ")"; }
   $.getJSON(uri, parm, data => {
     console.log("submitDataGet:" + JSON.stringify(data));
-    showAllFootprints();
+    showAllProducts();
   });
 }
